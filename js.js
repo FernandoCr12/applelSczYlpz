72d30d96c07c2772d47462f8966a6dc857db32f3
tortugaspizzaselalto <109116186+FernandoCr12@users.noreply.github.com>
2023-05-09 13:16:10 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+document.querySelector('.tarjeta-santacruz').addEventListener('click', function() {
+    window.location.href = '';
+});
+
+document.querySelector('.tarjeta-lapaz').addEventListener('click', function() {
+    window.location.href = '';
+});
